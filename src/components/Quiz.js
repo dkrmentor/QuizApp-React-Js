04b07a1d5b6141fe60bc4b2435340c6cc4753981
dkrmentor <dkrmentor@gmail.com>
2022-02-11 14:20:22 +0500

    quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,111 +1,122 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function Quiz() {
-  // {array}
   const questions = [
     {
-      question: "What is 2 + 2 ?",
-      answersOption: [
-        { answer: 5, isCorrect: false },
-        { answer: 8, isCorrect: false },
-        { answer: 4, isCorrect: true },
-        { answer: 33, isCorrect: false },
+      questionText: "What is 2 + 2 ?",
+      answerOptions: [
+        { answerText: "5", isCorrect: false },
+        { answerText: "8", isCorrect: false },
+        { answerText: "4", isCorrect: true },
+        { answerText: "3", isCorrect: false },
       ],
     },
     {
-      question: "What is 4 - 2 ?",
-      answersOption: [
-        { answer: 5, isCorrect: false },
-        { answer: 2, isCorrect: true },
-        { answer: 4, isCorrect: false },
-        { answer: 33, isCorrect: false },
+      questionText: "What is 4 - 2 ?",
+      answerOptions: [
+        { answerText: "5", isCorrect: false },
+        { answerText: "2", isCorrect: true },
+        { answerText: "4", isCorrect: false },
+        { answerText: "3", isCorrect: false },
       ],
     },
     {
-      question: " What is 2 * 2 ?",
-      answersOption: [
-        { answer: 4, isCorrect: true },
-        { answer: 8, isCorrect: false },
-        { answer: 4, isCorrect: false },
-        { answer: 33, isCorrect: false },
+      questionText: " What is 2 * 2 ?",
+      answerOptions: [
+        { answerText: "4", isCorrect: true },
+        { answerText: "8", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "3", isCorrect: false },
       ],
     },
     {
-      question: " What is 4 / 2 ?",
-      answersOption: [
-        { answer: 5, isCorrect: false },
-        { answer: 8, isCorrect: false },
-        { answer: 2, isCorrect: true },
-        { answer: 33, isCorrect: false },
+      questionText: " What is 4 / 2 ?",
+      answerOptions: [
+        { answerText: "5", isCorrect: false },
+        { answerText: "8", isCorrect: false },
+        { answerText: "2", isCorrect: true },
+        { answerText: "3", isCorrect: false },
       ],
     },
     {
-      question: "What is 4 % 2 ?",
-      answersOption: [
-        { answer: 5, isCorrect: false },
-        { answer: 0, isCorrect: true },
-        { answer: 4, isCorrect: false },
-        { answer: 33, isCorrect: false },
+      questionText: "What is 4 % 2 ?",
+      answerOptions: [
+        { answerText: "5", isCorrect: false },
+        { answerText: "0", isCorrect: true },
+        { answerText: "4", isCorrect: false },
+        { answerText: "3", isCorrect: false },
       ],
     },
     {
-      question: "ALgebra is the branch of Math?",
-      answersOption: [
-        { answer: "yes", isCorrect: true },
-        { answer: "no", isCorrect: false },
+      questionText: "ALgebra is the branch of Math?",
+      answerOptions: [
+        { answerText: "yes", isCorrect: true },
+        { answerText: "no", isCorrect: false },
       ],
     },
     {
-      question: "What is the value of x in x + 15 = 3 ?",
-      answersOption: [
-        { answer: 5, isCorrect: false },
-        { answer: -12, isCorrect: true },
-        { answer: 4, isCorrect: false },
-        { answer: 33, isCorrect: false },
+      questionText: "What is the value of x in x + 15 = 3 ?",
+      answerOptions: [
+        { answerText: 5, isCorrect: false },
+        { answerText: -12, isCorrect: true },
+        { answerText: 4, isCorrect: false },
+        { answerText: 33, isCorrect: false },
       ],
     },
     {
-      question: "What is the value of y in 4y = 24?<",
-      answersOption: [
-        { answer: 5, isCorrect: false },
-        { answer: 8, isCorrect: false },
-        { answer: 6, isCorrect: true },
-        { answer: 33, isCorrect: false },
+      questionText: "What is the value of y in 4y = 24?",
+      answerOptions: [
+        { answerText: 5, isCorrect: false },
+        { answerText: 8, isCorrect: false },
+        { answerText: 6, isCorrect: true },
+        { answerText: 33, isCorrect: false },
       ],
     },
   ];
-  const [currentQuestion, setcurrentQuestion] = useState(0);
+	
 
-  const handleAns = () =>{
-      const nxtquestion = currentQuestion + 1;
-      setcurrentQuestion(nextQuestion)
-  }
-  return (
-    <>
-      <div className="container">
-        {false ? (
-          <div className="score-section">
-            You scored 1 out of {questions.length}
-          </div>
-        ) : (
-          <>
-            <div className="question-section">
-              <div className="question-count">
-                <span>Q1</span> / {questions.length}
-              </div>
-              <div className="question-text">{questions[currentQuestion].question}</div>
-            </div>
-            <div className="answer-section">
-              {questions[currentQuestion].answersOption.map((answersOption) => {
-                <button> answersOption.answer </button>;
-              })}
-            </div>
-          </>
-        )}
-      </div>
-    
-    </>
-  );
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
-// {https://youtu.be/Lya-qYiDqIA}
+
+
+
+// https://www.youtube.com/watch?v=Lya-qYiDqIA&t=261s
